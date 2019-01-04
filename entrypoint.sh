4e0c8fa01cf7f0715bc83e96c2bc544c5861ecd7
monkey-company <43472405+monkey-company@users.noreply.github.com>
2019-01-04 23:28:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,10 @@
 echo $MODULES | sed 's/,/ /g' | a2enmod -q
 
 #run other script (cron, modules, etc)
-if [ ! -f "$SHFILE" ] ; then echo file not found ; else chmod +x $SHFILE && $SHFILE ; fi
+if [ ! -f "$SHFILE" ]
+then
+  echo "file not found"
+else
+  chmod +x $SHFILE
+  $SHFILE
+fi
