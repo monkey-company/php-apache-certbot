fcd5b2bfed397ac4e2f3c4fc96ee37216ded5a0c
monkey-company <43472405+monkey-company@users.noreply.github.com>
2019-01-04 02:26:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo $MODULES | sed 's/,/ /g' | a2enmod
-#apply changes
-service apache2 restart
 #run other script (cron, modules, etc)
 if [ ! -f "$SHFILE" ] ; then echo file not found ; else chmod +x $SHFILE && $SHFILE ; fi
