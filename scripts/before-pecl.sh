2a866c018795e06cbc7b48a626bb7c09efe345be
monkeyco <themonkeyandco@gmail.com>
2019-05-21 12:20:39 +0200

    Bsdiff native

--- scripts/before-pecl.sh
+++ scripts/before-pecl.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#bsdiff extension
-apt-get install bzip2 git -y
-git clone https://github.com/monkey-company/phpdiff.git
-cd phpdiff
-phpize
-export LDFLAGS="-lbz2" && ./configure --with-php-config=/php-path/bin/php-config
-make && make install
-
 #xdiff library
 cd /usr/src
 wget http://www.xmailserver.org/libxdiff-0.22.tar.gz
